ae7e9c457ac21ffdf51669936c23af8f3a1ec1b4
truongTonyS <kusang112003@gmail.com>
2024-01-14 07:08:09 -0500

    lab

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+import lab3 from './src/screens/lab3';
+
+
+const App = () => {
+  return <lab3></lab3>
+};
+
+export default App;
---
+++ src/screens/lab3/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const lab3 = () => {
+  return <Text>lab3</Text>;
+};
+
+export default lab3;
