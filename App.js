c6e9775f4a772cea86bafc99291de81ac98912aa
truongTonyS <kusang112003@gmail.com>
2024-01-15 05:29:41 -0500

    lab1

--- App.js
+++ App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import {Text} from 'react-native';
 import lab3 from './src/screens/lab3';
 
-
-const App = () => {
+ const App = () => {
   return <lab3></lab3>
 };
 
---
+++ src/screens/lab3/style.js
@@ -0,0 +1,11 @@
+import { Text,View } from "react-native";
+
+
+const lab3 = () => (
+
+    <View style={container}>
+        <Text style={Text}>React Native</Text> 
+    </View>
+)
+
+export default lab3;
